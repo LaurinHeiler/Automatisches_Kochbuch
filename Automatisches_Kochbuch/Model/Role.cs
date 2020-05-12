86192cf8dcacce6d72a0a6cca8027721fc819abd
Daniel ROMEN <daniel.romen@hak-bregenz.at>
2020-05-12 21:32:50 +0200

    Roles

---
+++ Automatisches_Kochbuch/Model/Role.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Automatisches_Kochbuch.Model
+{
+    public static class Role
+    {
+        public const string ADMIN = "admin";
+        public const string USER = "user";
+
+        private static ISet<string> _validRoles = new HashSet<string>()
+        {
+            ADMIN, USER
+        };
+
+        public static bool IsValid(string role)
+        {
+            return _validRoles.Contains(role.Trim().ToLower());
+        }
+    }
+}
